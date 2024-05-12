69d33f531ce748b6915eda208955e9d449a18e77
Sufiyan33 <basu700736@gmail.com>
2024-05-12 23:01:42 +0530

    Creating project to store files in S3 bucket

---
+++ S3/File-Storage-S3/src/main/java/com/filestorage/FileStorageS3Application.java
@@ -0,0 +1,13 @@
+package com.filestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageS3Application.class, args);
+	}
+
+}
---
+++ S3/File-Storage-S3/src/test/java/com/filestorage/FileStorageS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStorageS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
