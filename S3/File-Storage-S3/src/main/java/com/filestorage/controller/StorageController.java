49d5a2835aa03d4ab8ef800c88c8db228bb285db
Sufiyan33 <basu700736@gmail.com>
2024-05-14 16:36:12 +0530

    Get mapping endPoint for Downloading files

--- S3/File-Storage-S3/src/main/java/com/filestorage/controller/StorageController.java
+++ S3/File-Storage-S3/src/main/java/com/filestorage/controller/StorageController.java
@@ -2,12 +2,10 @@ package com.filestorage.controller;
 
 import com.filestorage.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -20,5 +18,17 @@ public class StorageController {
     @PostMapping("/upload")
     public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file){
         return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String filName){
+       byte[] data = service.downloadFile(filName);
+       ByteArrayResource resource = new ByteArrayResource(data);
+       return ResponseEntity
+               .ok()
+               .contentLength(data.length)
+               .header("Content-type", "application/octet-stream")
+               .header("Content-disposition", "attachment; fileName=\"" + filName + "\"")
+               .body(resource);
     }
 }
