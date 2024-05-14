98d3f42d42b9f02bfae4595840616cbfe733f5e8
Sufiyan33 <basu700736@gmail.com>
2024-05-14 17:08:21 +0530

    fixing typo in config class

--- S3/File-Storage-S3/src/main/java/com/filestorage/config/StorageConfig.java
+++ S3/File-Storage-S3/src/main/java/com/filestorage/config/StorageConfig.java
@@ -15,15 +15,15 @@ public class StorageConfig {
     @Value("${cloud.aws.credentials.access-key}")
     private String accessKey;
 
-    @Value("#{cloud.aws.credentials.secret-key}")
-    private String accessSecret;
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
 
     @Value("${cloud.aws.region.static}")
     private String region;
 
     @Bean
-    private AmazonS3 generateS3Client() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+    public AmazonS3 s3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
         return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
     }
 }
