0a8f8af81324352a295f8e04a0edafd72c8ae33b
Sufiyan33 <basu700736@gmail.com>
2024-05-14 16:09:26 +0530

    Method to upload file in bucket

---
+++ S3/File-Storage-S3/src/main/java/com/filestorage/service/StorageService.java
@@ -0,0 +1,44 @@
+package com.filestorage.service;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+@Service
+public class StorageService {
+
+    private static final Logger log = LoggerFactory.getLogger(StorageService.class);
+
+    @Value("${application.bucket.name}")
+    private String bucketName;
+
+    @Autowired
+    private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file){
+        File fileObj = convertMultiPartFiletoFile(file);
+        String fileName = System.currentTimeMillis()+ "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete();
+        return "File uploaded: " + fileName;
+    }
+
+    private File convertMultiPartFiletoFile(MultipartFile file){
+        File convertedFile = new File(file.getOriginalFilename());
+        try(FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        }catch (IOException e){
+            log.error("Error converting multipart file into file", e);
+        }
+        return convertedFile;
+    }
+}
